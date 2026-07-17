8cd99437e34227ccdf6a430094962a59833c29aa
Zart Zort <abdullah.durusoy31@gmail.com>
2026-07-17 03:16:03 +0300

    CSV Parser Not Done YETgit add .

--- libds.hpp
+++ libds.hpp
@@ -1,10 +1,44 @@
 #pragma once
 
+#include <cstdio>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
 class libds {
+  private:
   public:
     libds();
     ~libds();
 
+  // ----------------- CSV Parser -----------------------
+    struct StringRef {
+      const char *data;
+      size_t length;
+
+      std::string toString() const {
+        return std::string(data, length);
+      }
+    };
+    static int processCSV(const std::string& filepath) {
+      int fd = open(filepath.c_str(), O_RDONLY);
+      if (fd == -1)
+        return (std::cout << "Error while opening file!" << std::endl, -1);
+      struct stat st; fstat(fd, &st);
+      char *data = static_cast<char *>(mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
+      close(fd);
+      madvise(data, st.st_size, MADV_SEQUENTIAL);
+      munmap(data, st.st_size);
+    }
+  // ---------------------------------------------------
   /* Z Score Normalization  
    *
    double val = 1, mean = 3, std, 2;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#include <cstddef>
 #include <iostream>
 #include "libds.hpp"
 
+
 int main (int argc, char *argv[]) {
+  if (argv[1] != NULL)
   return 0;
 }
